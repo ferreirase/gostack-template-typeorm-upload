cad186889b0bd68dbed95f667c625ced589d0e32
Anderson Raphael <phaell.anderson09@gmail.com>
2020-06-10 16:07:53 -0300

    feature(upload): finished upload file and add

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,11 +1,14 @@
 /* eslint-disable no-param-reassign */
 import { Router } from 'express';
+import multer from 'multer';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
-// import ImportTransactionsService from '../services/ImportTransactionsService';
+import ImportTransactionsService from '../services/ImportTransactionsService';
+import uploadConfig from '../config/upload';
 
 const transactionsRouter = Router();
+const upload = multer(uploadConfig);
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = new TransactionsRepository();
@@ -34,12 +37,23 @@ transactionsRouter.post('/', async (request, response) => {
   return response.status(201).json(newTransaction);
 });
 
+/*
 transactionsRouter.delete('/:id', async (request, response) => {
   // TODO
-});
+}); */
 
-transactionsRouter.post('/import', async (request, response) => {
-  // TODO
-});
+transactionsRouter.post(
+  '/import',
+  upload.single('file'),
+  async (request, response) => {
+    const importTransactionsService = new ImportTransactionsService();
+
+    const newsTransactions = await importTransactionsService.execute({
+      fileName: request.file.filename,
+    });
+
+    return response.status(200).json(newsTransactions);
+  },
+);
 
 export default transactionsRouter;
